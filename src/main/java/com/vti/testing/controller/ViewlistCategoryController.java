94e173859124cdc29e86ead9ce9b6d38d01680b2
hung2803 <hungnt283@gmail.com>
2020-03-02 11:46:33 +0700

    create view list

---
+++ src/main/java/com/vti/testing/controller/ViewlistCategoryController.java
@@ -0,0 +1,97 @@
+package com.vti.testing.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+public class ViewlistCategoryController {
+	/**
+	 * This method is got all Questions.
+	 * 
+	 * @Description: .
+	 * @author: NNDuy
+	 * @create_date: Dec 7, 2019
+	 * @version: 1.0
+	 * @modifer: NNDuy
+	 * @modifer_date: Dec 7, 2019
+	 * @return List<Question>
+	 */
+	@GetMapping()
+	public ResponseEntity<?> getAllQuestions() {
+
+		return new ResponseEntity<>("ok", HttpStatus.OK);
+	}
+
+	/**
+	 * This method is got Question by ID.
+	 * 
+	 * @Description: .
+	 * @author: NNDuy
+	 * @create_date: Dec 7, 2019
+	 * @version: 1.0
+	 * @modifer: NNDuy
+	 * @modifer_date: Dec 7, 2019
+	 * @param id
+	 * @return Question
+	 */
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> getQuestionByID(@PathVariable(name = "id") short id) {
+		return new ResponseEntity<>("ok", HttpStatus.OK);
+	}
+
+	/**
+	 * This method is created Question.
+	 * 
+	 * @Description: .
+	 * @author: NNDuy
+	 * @create_date: Dec 7, 2019
+	 * @version: 1.0
+	 * @modifer: NNDuy
+	 * @modifer_date: Dec 7, 2019
+	 * @param form
+	 */
+	@PostMapping()
+	public ResponseEntity<?> createQuestion() {
+		return new ResponseEntity<>("Create success!", HttpStatus.OK);
+	}
+
+	/**
+	 * This method is updated Question.
+	 * 
+	 * @Description: .
+	 * @author: NNDuy
+	 * @create_date: Dec 13, 2019
+	 * @version: 1.0
+	 * @modifer: NNDuy
+	 * @modifer_date: Dec 13, 2019
+	 * @param form
+	 */
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<?> updateQuestion() {
+
+		return new ResponseEntity<>("Update success!", HttpStatus.OK);
+	}
+
+	/**
+	 * This method is deleted Question.
+	 * 
+	 * @Description: .
+	 * @author: NNDuy
+	 * @create_date: Dec 13, 2019
+	 * @version: 1.0
+	 * @modifer: NNDuy
+	 * @modifer_date: Dec 13, 2019
+	 * @param id
+	 */
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> deleteQuestion() {
+
+		return new ResponseEntity<>("Delete success!", HttpStatus.OK);
+	}
+
+}
+
