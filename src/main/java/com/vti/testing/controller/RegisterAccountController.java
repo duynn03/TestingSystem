4ae910743c0a7e97499cb9cb6e9be5fd3377f53f
tuananh1998 <tuananhcan63@gmail.com>
2020-03-02 11:20:24 +0700

    Create RegisterAccountController

---
+++ src/main/java/com/vti/testing/controller/RegisterAccountController.java
@@ -0,0 +1,16 @@
+//
+package com.vti.testing.controller;
+
+/**
+ * This class is . 
+ * 
+ * @Description: .
+ * @author: CTANH
+ * @create_date: Mar 2, 2020
+ * @version: 1.0
+ * @modifer: CTANH
+ * @modifer_date: Mar 2, 2020
+ */
+public class RegisterAccountController {
+
+}
